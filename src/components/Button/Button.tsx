2d83fbc59f7c6e8d03d0b7ad348d0ae3f8c3100c
avganka <karambasterg@gmail.com>
2023-08-18 19:39:08 +0300

    add Buttom component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,16 @@
+import {ButtonHTMLAttributes, ReactNode} from 'react';
+import styles from './Button.module.css';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>  {
+  children: ReactNode;
+}
+
+function Button({className, children, disabled, ...props}: ButtonProps) {
+  return (
+    <button className={`${styles.btn} ${className}`} {...props} disabled={disabled}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
