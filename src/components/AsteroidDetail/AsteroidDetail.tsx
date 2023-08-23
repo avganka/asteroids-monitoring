091d461fe87a1d85fcaffbbdc93f9946d722ef64
avganka <karambasterg@gmail.com>
2023-08-23 19:41:23 +0300

    redesign asteroid detail component

--- src/components/AsteroidDetail/AsteroidDetail.tsx
+++ src/components/AsteroidDetail/AsteroidDetail.tsx
@@ -2,10 +2,13 @@
 import styles from './AsteroidDetail.module.css';
 import {DistanceContext} from '@/context/DistanceContext';
 import {NearEarthObject} from '@/types';
-import {formatDistance} from '@/utils';
+import {formatDistance, formatWordDeclension} from '@/utils';
 import {format} from 'date-fns';
 import ru from 'date-fns/locale/ru';
 import {useContext} from 'react';
+import speedIcon from '../../../public/asteroid.svg';
+import distanceIcon from '../../../public/distance.svg';
+import OrbitIcon from '../../../public/orbit.svg';
 
 function AsteroidDetail({asteroid}: {asteroid: NearEarthObject}) {
   const units = useContext(DistanceContext);
@@ -19,6 +22,19 @@ function AsteroidDetail({asteroid}: {asteroid: NearEarthObject}) {
     return 0;
   });
 
+  const lunarDistanceLocale =
+    formatWordDeclension(parseInt(asteroid.close_approach_data[0].miss_distance.lunar), [
+      'лунная',
+      'лунных',
+      'лунных',
+    ]) +
+    ' ' +
+    formatWordDeclension(parseInt(asteroid.close_approach_data[0].miss_distance.lunar), [
+      'орбита',
+      'орбиты',
+      'орбит',
+    ]);
+
   return (
     <ul className={styles.approachesList}>
       {sortedApproaches.map((approach) => (
@@ -29,23 +45,35 @@ function AsteroidDetail({asteroid}: {asteroid: NearEarthObject}) {
                 locale: ru,
               })}
             </h2>
-            <p>
-              Скорость:{' '}
-              <span className={styles.asteroidValue}>
-                {formatDistance(approach.relative_velocity.kilometers_per_second)} км/с
-              </span>
-            </p>
-            <p>
-              Расстояние от Земли:{' '}
-              <span className={styles.asteroidValue}>
-                {units === 'kilometers'
-                  ? `${formatDistance(approach.miss_distance.kilometers)} км`
-                  : `${formatDistance(approach.miss_distance.lunar)} л.о.`}
-              </span>
-            </p>
-            <p>
-              Орбита: <span className={styles.asteroidValue}>{approach.orbiting_body}</span>
-            </p>
+            <div className={styles.asterodFeatures}>
+              <div className={styles.asteroidFeature}>
+                <svg className={styles.asteroidIcon} aria-hidden={true} width='50' height='50'>
+                  <use xlinkHref={`${speedIcon.src}#asteroid`}></use>
+                </svg>
+                Скорость
+                <span className={styles.asteroidValue}>
+                  {formatDistance(approach.relative_velocity.kilometers_per_second)} км/с
+                </span>
+              </div>
+              <div className={styles.asteroidFeature}>
+                <svg className={styles.asteroidIcon} aria-hidden={true} width='50' height='50'>
+                  <use xlinkHref={`${distanceIcon.src}#distance`}></use>
+                </svg>
+                Расстояние до земли
+                <span className={styles.asteroidValue}>
+                  {units === 'kilometers'
+                    ? `${formatDistance(approach.miss_distance.kilometers)} км`
+                    : `${formatDistance(approach.miss_distance.lunar)} ${lunarDistanceLocale}`}
+                </span>
+              </div>
+              <div className={styles.asteroidFeature}>
+                <svg className={styles.asteroidIcon} aria-hidden={true} width='50' height='50'>
+                  <use xlinkHref={`${OrbitIcon.src}#orbit`}></use>
+                </svg>
+                Орбита
+                <span className={styles.asteroidValue}>{approach.orbiting_body}</span>
+              </div>
+            </div>
           </article>
         </li>
       ))}
